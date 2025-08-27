7b309db3312f522cdea45578a731810e5b34be72
parsa hejazi <parsahejazi90@gmail.com>
2025-08-27 17:57:27 +0330

    its ok back up

--- python/project/project/test3.py
+++ python/project/project/test3.py
@@ -78,7 +78,7 @@ def sim(target1,target2):
                 ratings1.append(frrow)
                 ratings2.append(srrow)
                 
-                print(srow)
+                # print(srow)
     sqrr = [x**2 for x in ratings1]
     sqrr2 = [x**2 for x in ratings2]
     
@@ -102,26 +102,26 @@ def sim(target1,target2):
 def recomandation(target):
     x=[]
     for i in range(51 ):
-        print(i)
-        print(target)
-        y=sim(str(target),str(i))
-        print(y)
+        # print(i)
+        # print(target)
+        y= sim(str(target),str(i))
+        # print(y)
         x.append((y,i))
     x.sort(reverse=True)
-    print(x)
-    print("x : ",x[1])
+    # print(x)
+    # print("x : ",x[1])
     s,u=x[1]
+    # print("u",u)
     
     Rating_file = open('Ratings.txt')
     readR = Rating_file.readlines()
     ranked =[]
     ranked2 =[]
-    sim=np.array
+    # sim=np.array
     counter=0
     multi=0
     x=0
-    ratings1=[]
-    ratings2=[]
+    rec=[]
     # print(readR)1
     for item in readR:
         
@@ -130,31 +130,45 @@ def recomandation(target):
         if part[0]==target:
             # print("okay")
             ranked.append([int(part[1]),int(part[2])])
-        if part[0]==u:
-        # print("okay")
+        if part[0]==str(u):
+            # print("okay")
             ranked2.append([int(part[1]),int(part[2])])
     f_matrix=np.array(ranked)
     s_matrix=np.array(ranked2)
     # frow=f_matrix[:,0]
     # frrow=f_matrix[:,1]
+    # print(f_matrix)
+    # print(s_matrix)
     
     for i in range(s_matrix.shape[0]):
-        srow=s_matrix[i][0]
-        srrow=s_matrix[i][1]
-        multi=0
+        srow = s_matrix[i][0]
+        srrow = s_matrix[i][1]
+        multi = 0
+        found = False
         for j in range(f_matrix.shape[0]):
-            
-            if srow=if_matrix[j][0]:
+            if srow == f_matrix[j][0]:
+                found = True
+                break
+        if not found:
+            rec.append((srow, srrow))
+
                 
-                counter+=1
-                frrow=f_matrix[j][1]
-                multi=srrow*frrow
-                x+=multi
-                ratings1.append(frrow)
-                ratings2.append(srrow)
-   
+    rec.sort(key=lambda x: x[1], reverse=True) 
+    # print("rec",rec)
+    movie_file = open('movies.txt')
+    readM = movie_file.readlines()
+    for i in range(3):
+        # print("recomendation : " ,rec[i][0])
+        print("**********Recomandations********************************************************************")  
+        for item in readM:
+            partt= item.strip().split('\t')
+            # print(part[0])
+            if partt[0]==str(rec[i][0]):
+                
+                print(" Film : ",partt[1],'\t',partt[2],'\n')
+    
             
-        
+            
     
 
 while True :
